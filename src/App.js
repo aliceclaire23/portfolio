a1378be47d6c14a80caba05f6b17bed2f05f1616
Ecila23 <aliceclaireh@hotmail.co.uk>
2021-02-07 23:50:15 +0000

    create react app

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import './App.css';
+
+function App() {
+  return (
+      <div>
+        <p>Hi, I'm Alice</p>
+        <p>I'm a Yorkshire based software developer and general creative tinkerer. I sing and play guitar, and I like drawing and dabbling in different forms of digital art and animation</p>
+
+        <p>I can be found in these other internet places:</p>
+        <ul>
+            <li><a href="https://www.instagram.com/alice.claire.23/">Instagram</a></li>
+            <li><a href="https://www.instagram.com/alicedoesartythings/">Instagram - Art</a></li>
+            <li><a href="https://twitter.com/aliceclaire23">Twitter</a></li>
+            <li><a href="https://github.com/aliceclaire23">Github</a></li>
+        </ul>
+      </div>
+  );
+}
+
+export default App;
