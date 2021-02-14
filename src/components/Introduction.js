24fdd33005c77b7ad50edbdf4a3107378f92bfa1
Ecila23 <aliceclaireh@hotmail.co.uk>
2021-02-14 16:48:11 +0000

    rewrite intro and add portrait image

--- src/components/Introduction.js
+++ src/components/Introduction.js
@@ -1,11 +1,13 @@
 import React from 'react';
+import selfPortrait from '../images/self_portrait.png'
 
 const Introduction = () => {
     return (
         <div>
             <h1>Hi, I'm Alice.</h1>
-            {/* TODO: rewrite this paragraph */}
-            <p>I'm a Yorkshire based software developer and general creative tinkerer. I sing and play guitar, and I like drawing and dabbling in different forms of digital art and animation</p>
+            <p>I’m a Yorkshire based software developer and general creative tinkerer, and  my face looks a little like this:</p>
+            <img src={selfPortrait} alt="digital self portrait of Alice" className="self-portrait" />
+            <p>For more info, just keep scrolling. Just keep scrolling. Just keep scrolling, scrolling, scrolling. What do we do? We scroll, scroll. </p>
         </div>
     )
 };
