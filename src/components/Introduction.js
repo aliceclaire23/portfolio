f5abed0f6ef9c68851f82c7079316567ce265471
Ecila23 <aliceclaireh@hotmail.co.uk>
2021-02-21 19:15:15 +0000

    add image and styles

--- src/components/Introduction.js
+++ src/components/Introduction.js
@@ -3,11 +3,13 @@ import selfPortrait from '../images/self_portrait.png'
 
 const Introduction = () => {
     return (
-        <div>
-            <h1>Hi, I'm Alice.</h1>
-            <p>I’m a Yorkshire based software developer and general creative tinkerer, and  my face looks a little like this:</p>
-            <img src={selfPortrait} alt="digital self portrait of Alice" />
-            <p>For more info, just keep scrolling. Just keep scrolling. Just keep scrolling, scrolling, scrolling. What do we do? We scroll, scroll. </p>
+        <div className="section" >
+            <img src={selfPortrait} alt="digital self portrait of Alice" className="image image__portrait" />
+            <div className="section__text section--center" >
+                <h1 className="heading" >Hi, I'm Alice.</h1>
+                <p>I’m a Yorkshire based software developer and general creative tinkerer, and  my face looks a little like this:</p>
+                <p>For more info, just keep scrolling. Just keep scrolling. Just keep scrolling, scrolling, scrolling. What do we do? We scroll, scroll. </p>
+            </div>
         </div>
     )
 };
