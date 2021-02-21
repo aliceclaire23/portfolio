c539b228626e798453ddc639dae12cd0a2970dfb
Ecila23 <aliceclaireh@hotmail.co.uk>
2021-02-21 19:15:32 +0000

    add styles and remove whitespace

--- src/components/DeveloperJourney.js
+++ src/components/DeveloperJourney.js
@@ -2,23 +2,20 @@ import React from 'react';
 
 const DeveloperJourney = () => {
     return (
-        <div>
-            <h2>Developer Journey</h2>
-
-            <p>I started my journey with code on a Saturday afternoon in January 2018 with a Raspberry Pi,  some LEDs and dash of childlike fun and curiosity. I was hooked.</p>
-
-            <p>After that, I spent a lot of my time outside of my 9-5 teaching myself to code. I did online courses through Udemy, FreeCodeCamp and Codecademy; played about with some projects from the Raspberry Pi website and even started making a website for my friends’ wedding.</p>
-
-            <p>A year after I wrote my first while loop, I quit my job and applied to Northcoders, a full time coding bootcamp based in Leeds. I built on the knowledge and skills I already had, getting the chance to do things I couldn’t do with 2 hours hunched over the dining table on weekday evening:</p>
-            
-            <ul>
-                <li>Pair programming</li>
-                <li>Having my code reviewed</li>
-                <li>Project planning as a team</li>
-                <li>Making group decisions (and mistakes) and reflecting on them</li>
-            </ul>
-            
-            <p>Since then I’ve worked both for an agency and as an in-house developer on apps to aid learning, monitor and manage IOT devices,  and even editing pictures. It’s been a great journey so far, and I’m excited to see where it takes me next!</p>
+        <div className="section" >
+            <div className="section__text section--left" >
+                <h2 className="heading" >Developer Journey</h2>
+                <p>I started my journey with code on a Saturday afternoon in January 2018 with a Raspberry Pi,  some LEDs and dash of childlike fun and curiosity. I was hooked.</p>
+                <p>After that, I spent a lot of my time outside of my 9-5 teaching myself to code. I did online courses through Udemy, FreeCodeCamp and Codecademy; played about with some projects from the Raspberry Pi website and even started making a website for my friends’ wedding.</p>
+                <p>A year after I wrote my first while loop, I quit my job and applied to Northcoders, a full time coding bootcamp based in Leeds. I built on the knowledge and skills I already had, getting the chance to do things I couldn’t do with 2 hours hunched over the dining table on weekday evening:</p>
+                <ul>
+                    <li>Pair programming</li>
+                    <li>Having my code reviewed</li>
+                    <li>Project planning as a team</li>
+                    <li>Making group decisions (and mistakes) and reflecting on them</li>
+                </ul>
+                <p>Since then I’ve worked both for an agency and as an in-house developer on apps to aid learning, monitor and manage IOT devices,  and even editing pictures. It’s been a great journey so far, and I’m excited to see where it takes me next!</p>
+            </div>
         </div>
     )
 };
